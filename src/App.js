4fcbe0a25ce151f37277e6c1913bf0f6bc086f1c
absolitudo <absolitudo@gmail.com>
2018-08-01 13:04:39 +0200

    create basic structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
-import "./App.css";
+import Navigation from "./components/navigation";
 
 class App extends Component {
     render() {
-        return <div className="App">app</div>;
+        return (
+            <React.Fragment>
+                <Navigation />
+            </React.Fragment>
+        );
     }
 }
 
---
+++ src/components/navigation/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./navigation.css";
+
+const Navigation = props => <nav>this is the navigation</nav>;
+
+export default Navigation;
