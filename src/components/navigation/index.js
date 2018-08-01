9c24e3a22fb4d7c5e9da22260265234fd13bd215
absolitudo <absolitudo@gmail.com>
2018-08-01 17:49:55 +0200

    sign in

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 import "./navigation.css";
+import { connect } from "react-redux";
 
-const Navigation = props => <nav>this is the navigation</nav>;
+const Navigation = props => (
+    <nav>
+        <div className="user-status">
+            {props.userData.userName ? (
+                <LoggedIn userData={props.userData} />
+            ) : (
+                <LoggedOut userData={props.userData} />
+            )}
+        </div>
+    </nav>
+);
 
-export default Navigation;
+const LoggedIn = () => <p>logged in</p>;
+const LoggedOut = () => <a href="/login">SIGN IN</a>;
+
+const mapStateToProps = state => ({
+    userData: state.loginStatus
+});
+
+export default connect(mapStateToProps)(Navigation);
