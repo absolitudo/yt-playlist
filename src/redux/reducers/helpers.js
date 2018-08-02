6e7240077e1215f27de8dc0bccfb629eea92a0b6
absolitudo <absolitudo@gmail.com>
2018-08-02 15:45:21 +0200

    smaller changes

--- src/redux/reducers/helpers.js
+++ src/redux/reducers/helpers.js
@@ -1,7 +1,6 @@
 import Cookies from "js-cookie";
 import jwt from "jsonwebtoken";
-// "Ákos Kóczián"
-// "https://yt3.ggpht.com/-OFMAV2HFRYQ/AAAAAAAAAAI/AAAAAAAAAAA/kO-pFABvLzg/s88-c-k-no-mo-rj-c0xffffff/photo.jpg"
+
 const getUserData = () => {
     const userData = jwt.decode(Cookies.get("session_token"));
 
