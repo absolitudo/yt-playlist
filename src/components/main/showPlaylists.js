1719d8a4478a82d41378bafc5411f1173d4370e9
absolitudo <absolitudo@gmail.com>
2018-08-22 03:41:08 +0200

    improve playlist display styles and component structure

--- src/components/main/showPlaylists.js
+++ src/components/main/showPlaylists.js
@@ -3,27 +3,23 @@ import { connect } from "react-redux";
 import { handlePlaylistSelection } from "./helpers";
 
 const ShowPlaylists = props => (
-    <ul className="playlists">
+    <ul className="yt-items playlist">
         {props.items.map((data, index) => (
             <li
                 onClick={() => handlePlaylistSelection(props.dispatch, data.id)}
                 key={index}
             >
-                <div
-                    className="playlist-picture-conatiner"
-                    style={{
-                        width: data.snippet.thumbnails.default.width,
-                        height: data.snippet.thumbnails.default.height
-                    }}
-                >
+                <div className="yt-img-conatiner">
                     <img
-                        src={data.snippet.thumbnails.default.url}
+                        src={data.snippet.thumbnails.medium.url}
                         alt="playlist thumbnail"
                     />
                 </div>
-                <div className="playlist-info">
-                    <h3>{data.snippet.title}</h3>
-                    <p>#{data.snippet.channelTitle}</p>
+                <div className="yt-data-container">
+                    <h3 className="yt-data-title">{data.snippet.title}</h3>
+                    <div className="yt-data-metadata">
+                        #{data.snippet.channelTitle}
+                    </div>
                 </div>
             </li>
         ))}
